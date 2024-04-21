809c4ae64199c426312fc39ea215af7ea07ac67a
João Cravidão <joao_kravidao@hotmail.com>
2024-04-21 01:08:19 +0100

    first commit

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumns, userRows } from "../../datatablesource";
+import "./datatable.scss";
+import { Link } from "react-router-dom";
+
+const Datatable = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <span className="viewButton">View</span>
+            </Link>
+            <span
+              className="deleteButton"
+              onClick={() => handleDelete(params.row.id)}
+            >
+              Delete
+            </span>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="datatable">
+      <div className="datatableTitle">
+        Add New User
+        <Link to="/users/new" className="link">
+          Add New
+        </Link>
+      </div>
+      <DataGrid
+        className="datagrid"
+        rows={data}
+        columns={userColumns.concat(actionColumn)}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default Datatable;
